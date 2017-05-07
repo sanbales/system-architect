add50cf2509ca03194bc1bca39c31fee44387456
Santiago Balestrini-Robinson <santiago.balestrini@gtri.gatech.edu>
2017-05-07 13:56:42 -0400

    fixes to simple example fixture data generator

--- system_architect/system_architect/management/commands/add_fixture_data.py
+++ system_architect/system_architect/management/commands/add_fixture_data.py
@@ -4,17 +4,26 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from os.path import abspath, dirname, join
 
-from system_architect.models import Category, Function, Project, Scenario, System, WeightingScale
+from system_architect.models import Category, Function, FunctionRequires, Project, Scenario, System
 
 
 class Command(BaseCommand):
     help = 'Creates a superuser and other fixture data'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--remake',
+            action='store_true',
+            dest='remake',
+            default=False,
+            help='Delete old fixture data if it exists',
+        )
+
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("Running add_fixture_data"))
         self.make_superuser()
-        self.make_simple_example(force=options['force'])
-        self.make_naval_example(force=options['force'])
+        self.make_simple_example(remake=options['remake'])
+        self.make_naval_example(remake=options['remake'])
 
     def make_superuser(self):
         self.stdout.write(self.style.SUCCESS("  Creating Superuser:"))
@@ -26,11 +35,11 @@ class Command(BaseCommand):
         else:
             self.stdout.write(self.style.WARNING("    - Could not create superuser because settings.DEBUG is False"))
 
-    def make_simple_example(self, project_name="Make Toasts Great Again", force=False):
+    def make_simple_example(self, project_name="Make Toasts Great Again", remake=False):
         self.stdout.write(self.style.SUCCESS("  Creating '{}' Example".format(project_name)))
 
         if Project.objects.filter(name=project_name).count() > 0:
-            if force:
+            if remake:
                 Project.objects.filter(name=project_name).delete()
             else:
                 return
@@ -46,30 +55,56 @@ class Command(BaseCommand):
                                       description='Make toasts in less time and with less variability in duration')
         easier = project.add_function(name='Easier toasts', description='Make it easier to get toast golden brown')
 
+        criticality = self.make_criticality_scale(project)
+        satisfiability = self.make_satisfiability_scale(project)
+
         self.stdout.write(self.style.SUCCESS("    - Created high level functions"))
 
         # Better
         golden = project.add_function(name='Unburnt toasts', description='Make toasts be golden brown')
         robust = project.add_function(name='Toast robustly', description='Make toasting insensitive to type of bread,' +
                                                                          ' thickness, or temperature')
-        better.requires.add(golden, robust)
-        better.save()
+        for required in (golden, robust):
+            FunctionRequires.objects.create(requiring=better, required=required, project=project, scale=criticality)
 
         # Easier
         more = project.add_function(name='More toast',
                                     description='Allow for more than 2 toasts to be made at the same time')
         no_guess = project.add_function(name='Less guessing',
                                         description='Require less guessing from the user for toaster inputs')
-        easier.required.add(more, no_guess)
-        easier.save()
+        for required in (more, no_guess):
+            FunctionRequires.objects.create(requiring=easier, required=required, project=project, scale=criticality)
         self.stdout.write(self.style.SUCCESS("    - Created remaining functions"))
 
+        # TODO: finish example
         self.stdout.write(self.style.SUCCESS("    - Finish creating fixture data for '{}'".format(project_name)))
 
-    def make_naval_example(self, project_name="Naval Example", folder='naval_example', force=False):
+    @staticmethod
+    def make_criticality_scale(project):
+        criticality = project.add_scale(name='Criticality',
+                                        description="A measure of how critical one entity is to another.")
+        criticality.add_level('Cannot Be Achieved Without', 1.0)
+        criticality.add_level('Seriously Jeopardized Without', 0.8)
+        criticality.add_level('Somewhat Jeopardized Without', 0.6)
+        criticality.add_level('Minimally Jeopardized Without', 0.3)
+        criticality.add_level('Practically Not Jeopardized Without', 0.1)
+        criticality.add_level('Not Applicable', 0.0)
+        return criticality
+
+    @staticmethod
+    def make_satisfiability_scale(project):
+        satisfiability = project.add_scale(name='Satisfiability',
+                                           description="A measure of how well something can satisfy something else.")
+        satisfiability.add_level('Completely satisfies the function under all circumstances', 1.0)
+        satisfiability.add_level('Satisfies the function under most circumstances', 0.8)
+        satisfiability.add_level('Satisfies the function under some circumstances', 0.2)
+        satisfiability.add_level('Does not satisfies the function', 0.0)
+        return satisfiability
+
+    def make_naval_example(self, project_name="Naval Example", folder='naval_example', remake=False):
 
         if Project.objects.filter(name=project_name).count() > 0:
-            if force:
+            if remake:
                 Project.objects.filter(name=project_name).delete()
             else:
                 return
@@ -130,18 +165,6 @@ class Command(BaseCommand):
         qfd.add_level('Low', 1.0)
         qfd.add_level('N/A', 0.0)
 
-        criticality = project.add_scale(name='Criticality',
-                                        description="A measure of how critical one entity is to another.")
-        criticality.add_level('Cannot Be Achieved Without', 1.0)
-        criticality.add_level('Seriously Jeopardized Without', 0.8)
-        criticality.add_level('Somewhat Jeopardized Without', 0.6)
-        criticality.add_level('Minimally Jeopardized Without', 0.3)
-        criticality.add_level('Practically Not Jeopardized Without', 0.1)
-        criticality.add_level('Not Applicable', 0.0)
+        criticality = self.make_criticality_scale(project)
 
-        satisfiability = project.add_scale(name='Satisfiability',
-                                           description="A measure of how well something can satisfy something else.")
-        satisfiability.add_level('Completely satisfies the function under all circumstances', 1.0)
-        satisfiability.add_level('Satisfies the function under most circumstances', 0.8)
-        satisfiability.add_level('Satisfies the function under some circumstances', 0.2)
-        satisfiability.add_level('Does not satisfies the function', 0.0)
+        satisfiability = self.make_satisfiability_scale(project)
