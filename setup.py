ed8e0a0e71179e5bc6e845d6831684531e3c4ba0
Santiago Balestrini-Robinson <santiago.balestrini@gtri.gatech.edu>
2017-05-14 08:59:41 -0400

    updated .travis.yml to define django versions better

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Framework :: Django',
         'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
     ],
